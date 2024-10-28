97f0b450bb43d128f98bf0af9a918cd091e3e355
Sergio del Amo <sergio.delamo@softamo.com>
2024-10-28 18:28:37 +0100

    use Duration.from().getSeconds()

--- session/src/test/groovy/io/micronaut/session/http/HttpSessionConfigurationSpec.groovy
+++ session/src/test/groovy/io/micronaut/session/http/HttpSessionConfigurationSpec.groovy
@@ -3,6 +3,7 @@ package io.micronaut.session.http
 import io.micronaut.context.ApplicationContext
 import spock.lang.Specification
 
+import java.time.Duration
 import java.time.temporal.ChronoUnit
 
 class HttpSessionConfigurationSpec extends Specification {
@@ -12,10 +13,10 @@ class HttpSessionConfigurationSpec extends Specification {
         ApplicationContext ctx = ApplicationContext.run('micronaut.session.http.cookie-max-age': '365d')
 
         expect:
-            ctx.getBean(HttpSessionConfiguration)
+        Duration.from(ctx.getBean(HttpSessionConfiguration)
             .getCookieMaxAge()
             .get()
-            .get(ChronoUnit.SECONDS) == 365 * 24 * 60 * 60
+            ).getSeconds() == 365 * 24 * 60 * 60
 
         cleanup:
         ctx.close()
