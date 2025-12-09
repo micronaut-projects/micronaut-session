ceabc3c8a1b51fbc5f34a2f72da669ea8c19688b
Sergio del Amo <sergio.delamo@softamo.com>
2025-12-09 10:36:00 +0100

    use jspecify nullability annotations

--- session/src/test/groovy/io/micronaut/session/http/SessionBindingSpec.groovy
+++ session/src/test/groovy/io/micronaut/session/http/SessionBindingSpec.groovy
@@ -17,7 +17,7 @@ package io.micronaut.session.http
 
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.annotation.Requires
-import io.micronaut.core.annotation.Nullable
+import org.jspecify.annotations.Nullable
 import io.micronaut.http.HttpHeaders
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
